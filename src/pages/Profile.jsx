ef3ccd29ff9527875290015ebd37726d69f36fa0
Kaung Sithu <kgcthu.kev@gmail.com>
2021-12-21 14:41:42 +0630

    user sign in

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,13 @@
-import SignIn from './SignIn'
+import { getAuth } from 'firebase/auth'
+import { useEffect, useState } from 'react'
 
 function Profile() {
-  return (
-    <>
-      <SignIn />
-    </>
-  )
+  const [user, setUser] = useState(null)
+  const auth = getAuth()
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [])
+  return user ? <h1>{user.displayName}</h1> : 'not logged in'
 }
 
 export default Profile
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
@@ -19,6 +20,23 @@ function SignIn() {
       [e.target.id]: e.target.value,
     }))
   }
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      if (userCredential.user) {
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className='pageContainer'>
@@ -27,7 +45,7 @@ function SignIn() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type='email'
               className='emailInput'
