a4724fb108790d4077a43bd76ce42c2c0ed6409a
Kaung Sithu <kgcthu.kev@gmail.com>
2021-12-22 19:52:37 +0630

    ListingItem

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -11,6 +11,7 @@ import {
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import ListingItem from '../components/ListingItem'
 import Spinner from '../components/Spinner'
 import { db } from '../firebase.config'
 
@@ -69,7 +70,10 @@ function Category() {
         <>
           <main className='categoryListings'>
             {listings.map((listing) => (
-              <h3 key={listing.id}>{listing.data.name}</h3>
+              <ListingItem
+                key={listing.id}
+                listing={listing.data}
+              ></ListingItem>
             ))}
           </main>
         </>
