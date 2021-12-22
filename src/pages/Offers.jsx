00c8b36b68b05befaf2658f61193fa15838609c1
Kaung Sithu <kgcthu.kev@gmail.com>
2021-12-22 20:01:24 +0630

    Offers page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,7 +1,81 @@
+import {
+  collection,
+  doc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  where,
+} from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import ListingItem from '../components/ListingItem'
+import Spinner from '../components/Spinner'
+import { db } from '../firebase.config'
+
 function Offers() {
+  const [listings, setLisings] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const params = useParams()
+
+  const fetchListings = async () => {
+    try {
+      // Get ref
+      const listingsRef = collection(db, 'listings')
+
+      // create query
+      const q = query(
+        listingsRef,
+        where('offer', '==', true),
+        orderBy('timestamp', 'desc'),
+        limit(10)
+      )
+
+      // execute query
+      const querySnap = await getDocs(q)
+      const listings = []
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+
+      setLisings(listings)
+      setLoading(false)
+    } catch (error) {
+      toast.error('Could not fetch the data')
+    }
+  }
+
+  useEffect(() => {
+    fetchListings()
+  }, [])
   return (
-    <div>
-      <h1>Offers</h1>
+    <div className='category'>
+      <header>
+        <p className='pageHeader'>Offers</p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main className='categoryListings'>
+            {listings.map((listing) => (
+              <ListingItem
+                key={listing.id}
+                listing={listing.data}
+              ></ListingItem>
+            ))}
+          </main>
+        </>
+      ) : (
+        <p>There are no current offer.</p>
+      )}
     </div>
   )
 }
