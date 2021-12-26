743730400c93a7fe7ee951b8761157361f77dc94
Kaung Sithu <kgcthu.kev@gmail.com>
2021-12-26 15:42:11 +0630

    Map leaflet to Listing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 import { db } from '../firebase.config'
 import shareIcon from '../assets/svg/shareIcon.svg'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 function Listing() {
   const [listing, setListing] = useState({})
@@ -85,6 +86,27 @@ function Listing() {
           <li>{listing.furnished && 'Furnished'}</li>
         </ul>
         <p className='listingLocationTitle'>Location</p>
+        {/* Map */}
+        <div className='leafletContainer'>
+          <MapContainer
+            style={{ height: '100%', width: '100%' }}
+            center={[listing.geoLocation.lat, listing.geoLocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+            />
+
+            <Marker
+              position={[listing.geoLocation.lat, listing.geoLocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
             to={`/contact/${listing.userRef}?listingName=${listing.name}`}
@@ -99,3 +121,5 @@ function Listing() {
 }
 
 export default Listing
+
+// https://stackoverflow.com/questions/67552020/how-to-fix-error-failed-to-compile-node-modules-react-leaflet-core-esm-pat
