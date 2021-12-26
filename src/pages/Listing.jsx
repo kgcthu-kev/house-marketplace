f0bb7c9e9fa0cafcfb8582ee45ffcee7a638a347
Kaung Sithu <kgcthu.kev@gmail.com>
2021-12-26 19:36:52 +0630

    Image Swiper

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,6 +6,10 @@ import Spinner from '../components/Spinner'
 import { db } from '../firebase.config'
 import shareIcon from '../assets/svg/shareIcon.svg'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
   const [listing, setListing] = useState({})
@@ -37,6 +41,19 @@ function Listing() {
   return (
     <main>
       {/* Slider */}
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imageUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imageUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+              }}
+              className='swiperSlideDiv'
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <div
         className='shareIconDiv'
         onClick={() => {
